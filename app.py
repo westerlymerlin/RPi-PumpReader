bd58b9e79551839b9276001f9f736100e1483057
garyt <gary@twinn.net>
2023-03-04 13:48:03 +0000

    Version 1.3.4 Added version to Status Pages

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, jsonify, request
 from pumpclass import *
+from settings import version
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def index():
         log = f.readline()
     f.close()
     cputemperature = round(float(log)/1000, 1)
-    return render_template('index.html', pressures=httpstatus(), cputemperature=cputemperature)
+    return render_template('index.html', pressures=httpstatus(), cputemperature=cputemperature, version=version)
 
 
 @app.route('/api', methods=['POST'])
@@ -48,7 +49,7 @@ def showplogs():
     f.close()
     log.reverse()
     logs = tuple(log)
-    return render_template('logs.html', rows=logs, log='X-Y log', cputemperature=cputemperature)
+    return render_template('logs.html', rows=logs, log='X-Y log', cputemperature=cputemperature, version=version)
 
 
 @app.route('/guaccesslog')
@@ -62,7 +63,7 @@ def showgalogs():
     f.close()
     log.reverse()
     logs = tuple(log)
-    return render_template('logs.html', rows=logs, log='gunicorn access log', cputemperature=cputemperature)
+    return render_template('logs.html', rows=logs, log='gunicorn access log', cputemperature=cputemperature, version=version)
 
 
 @app.route('/guerrorlog')
@@ -76,7 +77,7 @@ def showgelogs():
     f.close()
     log.reverse()
     logs = tuple(log)
-    return render_template('logs.html', rows=logs, log='gunicorn error log', cputemperature=cputemperature)
+    return render_template('logs.html', rows=logs, log='gunicorn error log', cputemperature=cputemperature, version=version)
 
 
 @app.route('/uscHALT')
