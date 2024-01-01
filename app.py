7f10786b97809caef9d82c51694a59b2adb1eb2a
Gary Twinn <gary@twinn.net>
2024-01-01 14:40:30 +0000

    2.0.2 Updates following static code analysis

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+"""
+This is the main flask application - called by Gunicorn
+"""
 import os
 import subprocess
+from threading import Timer
 from flask import Flask, render_template, jsonify, request
-from pumpclass import *
+from pumpclass import httpstatus, temperature, pressures, pyrometer
 from logmanager import  logger
-from settings import version
-
+from settings import settings, VERSION
 
 
 app = Flask(__name__)
@@ -12,87 +15,93 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open(settings['cputemp'], 'r') as f:
+    """Main web page handler, shows status page via the index.html template"""
+    with open(settings['cputemp'], 'r', encoding='utf-8') as f:
         log = f.readline()
     f.close()
     cputemperature = round(float(log)/1000, 1)
-    return render_template('index.html', pressures=httpstatus(), cputemperature=cputemperature, version=version)
+    return render_template('index.html', pressures=httpstatus(), cputemperature=cputemperature, version=VERSION)
 
 
 @app.route('/api', methods=['POST'])
 def api():
+    """API Endpoint for programatic access - needs request data to be posted in a json file"""
     try:
         item = request.json['item']
         if item == 'gettemperature':
             return jsonify(temperature()), 201
-        elif item == 'getpressures':
+        if item == 'getpressures':
             return jsonify(pressures()), 201
-        elif item == 'resetmax':
+        if item == 'resetmax':
             pyrometer.resetmax()
             return jsonify(pressures()), 201
-        elif item == 'laser':
+        if item == 'laser':
             if request.json['command'] == "on":
                 pyrometer.laseron()
             else:
                 pyrometer.laseroff()
             return jsonify(temperature()), 201
-        elif item == 'restart':
+        if item == 'restart':
             if request.json['command'] == 'pi':
                 logger.info('Restart command recieved: system will restart in 15 seconds')
                 timerthread = Timer(15, reboot)
                 timerthread.start()
                 return jsonify(pressures()), 201
-        else:
-            return "badly formed json message - item not found", 201
+        logger.warning('API: badly formed json message')
+        return 'badly formed json message - item not found', 201
     except KeyError:
         return "badly formed json message", 201
 
 
 @app.route('/pylog')
 def showplogs():
-    with open(settings['cputemp'], 'r') as f:
+    """Displays the application log file via the logs.html template"""
+    with open(settings['cputemp'], 'r', encoding='utf-8') as f:
         log = f.readline()
     f.close()
     cputemperature = round(float(log)/1000, 1)
-    with open(settings['logfilepath'], 'r') as f:
+    with open(settings['logfilepath'], 'r', encoding='utf-8') as f:
         log = f.readlines()
     f.close()
     log.reverse()
     logs = tuple(log)
-    return render_template('logs.html', rows=logs, log='Pump Reader Application Log', cputemperature=cputemperature, version=version)
+    return render_template('logs.html', rows=logs, log='Pump Reader Application Log', cputemperature=cputemperature, version=VERSION)
 
 
 @app.route('/guaccesslog')
 def showgalogs():
-    with open(settings['cputemp'], 'r') as f:
+    """Displays the Gunicorn access log file via the logs.html template"""
+    with open(settings['cputemp'], 'r', encoding='utf-8') as f:
         log = f.readline()
     f.close()
     cputemperature = round(float(log)/1000, 1)
-    with open(settings['gunicornpath'] + 'gunicorn-access.log', 'r') as f:
+    with open(settings['gunicornpath'] + 'gunicorn-access.log', 'r', encoding='utf-8') as f:
         log = f.readlines()
     f.close()
     log.reverse()
     logs = tuple(log)
-    return render_template('logs.html', rows=logs, log='gunicorn access log', cputemperature=cputemperature, version=version)
+    return render_template('logs.html', rows=logs, log='gunicorn access log', cputemperature=cputemperature, version=VERSION)
 
 
 @app.route('/guerrorlog')
 def showgelogs():
-    with open(settings['cputemp'], 'r') as f:
+    """Displays the Gunicorn error log file via the logs.html template"""
+    with open(settings['cputemp'], 'r', encoding='utf-8') as f:
         log = f.readline()
     f.close()
     cputemperature = round(float(log)/1000, 1)
-    with open(settings['gunicornpath'] + 'gunicorn-error.log', 'r') as f:
+    with open(settings['gunicornpath'] + 'gunicorn-error.log', 'r', encoding='utf-8') as f:
         log = f.readlines()
     f.close()
     log.reverse()
     logs = tuple(log)
-    return render_template('logs.html', rows=logs, log='gunicorn error log', cputemperature=cputemperature, version=version)
+    return render_template('logs.html', rows=logs, log='gunicorn error log', cputemperature=cputemperature, version=VERSION)
 
 
 @app.route('/syslog')  # display the raspberry pi system log
 def showslogs():
-    with open(settings['cputemp'], 'r') as f:
+    """Displays the last 200 lines of the system log via the logs.html template"""
+    with open(settings['cputemp'], 'r', encoding='utf-8') as f:
         log = f.readline()
     f.close()
     cputemperature = round(float(log)/1000, 1)
@@ -100,11 +109,12 @@ def showslogs():
                            stdout=subprocess.PIPE).stdout.read().decode(encoding='utf-8')
     logs = log.split('\n')
     logs.reverse()
-    return render_template('logs.html', rows=logs, log='System Log', cputemperature=cputemperature, version=version)
+    return render_template('logs.html', rows=logs, log='System Log', cputemperature=cputemperature, version=VERSION)
 
 
 def reboot():
-    print('System is restarting now')
+    """Reboots the Raspberry Pi"""
+    logger.warning('System is restarting now')
     os.system('sudo reboot')
 
 
--- settings.py
+++ settings.py
@@ -1,24 +1,22 @@
+"""
+Settings module, reads the settings from a settings.json file. If it does not exist or a new setting
+has appeared it will creat from the defaults in the initialise function.
+"""
 import json
 from datetime import datetime
-import base64
 
-version = '2.0.1'
-
-
-def json_encoder(byte_obj):
-    if isinstance(byte_obj, (bytes, bytearray)):
-        # File Bytes to Base64 Bytes then to String
-        return base64.b64encode(byte_obj).decode('utf-8')
-    raise ValueError('No encoding handler for data type ' + type(byte_obj))
+VERSION = '2.0.2'
 
 
 def writesettings():
+    """Write settings to json file"""
     settings['LastSave'] = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-    with open('settings.json', 'w') as outfile:
-        json.dump(settings, outfile, indent=4, sort_keys=True, default=json_encoder)
+    with open('settings.json', 'w', encoding='utf-8') as outfile:
+        json.dump(settings, outfile, indent=4, sort_keys=True)
 
 
-def initialise():  # These are the default values written to the settings.json file the first time the app is run
+def initialise():
+    """These are the default values written to the settings.json file the first time the app is run"""
     isettings = {'LastSave': '01/01/2000 00:00:01',
                  'cputemp': '/sys/class/thermal/thermal_zone0/temp',
                  'gunicornpath': './logs/',
@@ -26,57 +24,42 @@ def initialise():  # These are the default values written to the settings.json f
                  'ion-port': '/dev/ttyUSB2',
                  'ion-speed': 9600,
                  'ion-start': 9,
-                 'ion-string1': b'~ 05 0B 00\r',
+                 'ion-string1': 'fiAwNSAwQiAwMA0=',  # base64 encoded
                  'logappname': 'Pumpreader-Py',
                  'logfilepath': './logs/pumpreader.log',
-                 'pyro-laseroff':b'\xA5\x00\xA5',
-                 'pyro-laseron': b'\xA5\x01\xA4',
+                 'pyro-laseroff': 'pQCl',  # base64 encoded
+                 'pyro-laseron': 'pQGk',  # base64 encoded
                  'pyro-port': '/dev/ttyUSB3',
                  'pyro-speed': 115200,
-                 'pyro-readlaser': b'\x25',
-                 'pyro-readtemp': b'\x01',
+                 'pyro-readlaser': 'JQ==',  # base64 encoded
+                 'pyro-readtemp': 'AQ==',  # base64 encoded
                  'tank-length': 16,
                  'tank-port': '/dev/ttyUSB1',
                  'tank-speed': 9600,
                  'tank-start': 5,
-                 'tank-string1': b'PR1\r',
-                 'tank-string2': b'\x05',
+                 'tank-string1': 'UFIxDQ==',  # base64 encoded
+                 'tank-string2': 'BQ==',  # base64 encoded
                  'turbo-length': 16,
                  'turbo-port': '/dev/ttyUSB0',
                  'turbo-speed': 9600,
                  'turbo-start': 5,
-                 'turbo-string1': b'PR1\r',
-                 'turbo-string2': b'\x05'}
+                 'turbo-string1': 'UFIxDQ==',  # base64 encoded
+                 'turbo-string2': 'BQ=='  # base64 encoded
+                 }
     return isettings
 
 def readsettings():
+    """Read the json file"""
     try:
-        with open('settings.json') as json_file:
+        with open('settings.json', 'r', encoding='utf-8') as json_file:
             jsettings = json.load(json_file)
-            # decode the encoded values
-            if 'ion-string1' in jsettings.keys() : jsettings['ion-string1'] = base64.b64decode(jsettings['ion-string1'])
-            if 'pyro-laseroff' in jsettings.keys(): jsettings['pyro-laseroff'] = base64.b64decode(
-                jsettings['pyro-laseroff'])
-            if 'pyro-laseron' in jsettings.keys(): jsettings['pyro-laseron'] = base64.b64decode(
-                jsettings['pyro-laseron'])
-            if 'pyro-readlaser' in jsettings.keys(): jsettings['pyro-readlaser'] = base64.b64decode(
-                jsettings['pyro-readlaser'])
-            if 'pyro-readtemp' in jsettings.keys(): jsettings['pyro-readtemp'] = base64.b64decode(
-                jsettings['pyro-readtemp'])
-            if 'tank-string1' in jsettings.keys(): jsettings['tank-string1'] = base64.b64decode(
-                jsettings['tank-string1'])
-            if 'tank-string2' in jsettings.keys(): jsettings['tank-string2'] = base64.b64decode(
-                jsettings['tank-string2'])
-            if 'turbo-string1' in jsettings.keys(): jsettings['turbo-string1'] = base64.b64decode(
-                jsettings['turbo-string1'])
-            if 'turbo-string2' in jsettings.keys(): jsettings['turbo-string2'] = base64.b64decode(
-                jsettings['turbo-string2'])
             return jsettings
     except FileNotFoundError:
         print('File not found')
         return {}
 
 def loadsettings():
+    """Replace the default settings with thsoe from the json files"""
     global settings
     settingschanged = 0
     fsettings = readsettings()
@@ -84,7 +67,7 @@ def loadsettings():
         try:
             settings[item] = fsettings[item]
         except KeyError:
-            print('settings[%s] Not found in json file using default' % item)
+            print('settings[%s] Not found in json file using default', item)
             settingschanged = 1
     if settingschanged == 1:
         writesettings()
